a56b75f5f9cd07a2eae3cf6a52dd3d54518aafaf
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2015-08-08 13:20:52 +0100

    first commit

---
+++ PortalsWithRenderTargets/Assets/Scripts/CamController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Simple script to allow WASD camera controls with mouse look
+public class CamController : MonoBehaviour {
+	public Camera MainCamera = null;
+	public Camera PortalCamera = null; // Currently just set to Portal Camera 1
+	public float LookSpeed = 1.0f;
+	public float MoveSpeed = 1.0f;
+	
+	private void Update () {
+		// Switch cameras (to view the scene from the Portal cameras perspective)
+		if (Input.GetKeyDown (KeyCode.C)) {
+			MainCamera.enabled = !MainCamera.enabled;
+			PortalCamera.enabled = !PortalCamera.enabled;
+		}
+
+		// Simple Mouse Look
+		if (Input.GetMouseButton (1)) {
+			float xAxis = Input.GetAxis ("Mouse X");
+			float yAxis = Input.GetAxis ("Mouse Y");
+
+			Quaternion yRot = Quaternion.AngleAxis (xAxis * LookSpeed * Time.deltaTime, Vector3.up);
+			Quaternion xRot = Quaternion.AngleAxis (-yAxis * LookSpeed * Time.deltaTime, transform.right);
+
+			transform.localRotation = yRot * xRot * transform.localRotation;
+		}
+	
+		// Basic Movement
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += (transform.forward * MoveSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= (transform.forward * MoveSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position -= (transform.right * MoveSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += (transform.right * MoveSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.E)) {
+			transform.position += (Vector3.up * MoveSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.Q)) {
+			transform.position -= (Vector3.up * MoveSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ PortalsWithRenderTargets/Assets/Scripts/Portal.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+	public Camera MainCamera = null;
+	// One to render to texture, and another to render normally to switch between
+	public Camera[] PortalCameras = null;
+	public Transform Source = null;
+	public Transform Destination = null;
+	public float NearClipTolerance = 0.0f;
+
+	private void LateUpdate() {
+		foreach (Camera portalCamera in PortalCameras) {
+			// Calculate translation and rotation of MainCamera in Source space
+			Vector3 cameraInSourceSpace = Source.InverseTransformPoint (MainCamera.transform.position);
+			Quaternion cameraInSourceSpaceRot = Quaternion.Inverse (Source.rotation) * MainCamera.transform.rotation;
+
+			// Note: Valid if PortalCamera and MainCamera are each in space of/children
+			// of Source and Destination transforms respectively
+			// portalCamera.transform.localPosition = cameraInSourceSpace;
+			// portalCamera.transform.localRotation = cameraInSourceSpaceRot;
+
+			// Transform Portal Camera to World Space relative to Destination transform,
+			// matching the Main Camera position/orientation
+			portalCamera.transform.position = Destination.TransformPoint (cameraInSourceSpace);
+			portalCamera.transform.rotation = Destination.rotation * cameraInSourceSpaceRot;
+
+			portalCamera.fieldOfView = MainCamera.fieldOfView;
+			portalCamera.aspect = MainCamera.aspect;
+			// Note: Optimisation to adjust the near clip as the camera gets further away from the Portal
+			// portalCamera.nearClipPlane = Vector3.Magnitude(Source.position - MainCamera.transform.position) - NearClipTolerance;
+		}
+	}
+}
