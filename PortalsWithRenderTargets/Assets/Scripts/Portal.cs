13f4a67da8d9facd3db4fb3f5b566ed689db785f
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2017-01-31 23:36:41 +0000

    add oblique matrix calculation to better handle culling of objects between portal camera and portal

--- PortalsWithRenderTargets/Assets/Scripts/Portal.cs
+++ PortalsWithRenderTargets/Assets/Scripts/Portal.cs
@@ -3,32 +3,29 @@ using System.Collections;
 
 public class Portal : MonoBehaviour {
 	public Camera MainCamera = null;
-	// One to render to texture, and another to render normally to switch between
+	// One to render to texture, and another to render normally to switch between (preview)
 	public Camera[] PortalCameras = null;
 	public Transform Source = null;
 	public Transform Destination = null;
-	public float NearClipTolerance = 0.0f;
-
+	
 	private void LateUpdate() {
 		foreach (Camera portalCamera in PortalCameras) {
 			// Calculate translation and rotation of MainCamera in Source space
-			Vector3 cameraInSourceSpace = Source.InverseTransformPoint (MainCamera.transform.position);
-			Quaternion cameraInSourceSpaceRot = Quaternion.Inverse (Source.rotation) * MainCamera.transform.rotation;
-
-			// Note: Valid if PortalCamera and MainCamera are each in space of/children
-			// of Source and Destination transforms respectively
-			// portalCamera.transform.localPosition = cameraInSourceSpace;
-			// portalCamera.transform.localRotation = cameraInSourceSpaceRot;
+			Vector3 cameraPositionInSourceSpace = Source.InverseTransformPoint(MainCamera.transform.position);
+			Quaternion cameraRotationInSourceSpace = Quaternion.Inverse(Source.rotation) * MainCamera.transform.rotation;
 
 			// Transform Portal Camera to World Space relative to Destination transform,
 			// matching the Main Camera position/orientation
-			portalCamera.transform.position = Destination.TransformPoint (cameraInSourceSpace);
-			portalCamera.transform.rotation = Destination.rotation * cameraInSourceSpaceRot;
+			portalCamera.transform.position = Destination.TransformPoint(cameraPositionInSourceSpace);
+			portalCamera.transform.rotation = Destination.rotation * cameraRotationInSourceSpace;
 
-			portalCamera.fieldOfView = MainCamera.fieldOfView;
-			portalCamera.aspect = MainCamera.aspect;
-			// Note: Optimisation to adjust the near clip as the camera gets further away from the Portal
-			// portalCamera.nearClipPlane = Vector3.Magnitude(Source.position - MainCamera.transform.position) - NearClipTolerance;
+			// Calculate clip plane for portal (for culling of objects inbetween destination camera and portal)
+			Vector4 clipPlaneWorldSpace = new Vector4( Destination.forward.x, Destination.forward.y, Destination.forward.z, Vector3.Dot( Destination.position, -Destination.forward ) );
+			Vector4 clipPlaneCameraSpace = Matrix4x4.Transpose( portalCamera.cameraToWorldMatrix ) * clipPlaneWorldSpace;
+
+			// Update projection based on new clip plane
+			// Note: http://aras-p.info/texts/obliqueortho.html and http://www.terathon.com/lengyel/Lengyel-Oblique.pdf
+			portalCamera.projectionMatrix = portalCamera.CalculateObliqueMatrix( clipPlaneCameraSpace );
 		}
 	}
 }
