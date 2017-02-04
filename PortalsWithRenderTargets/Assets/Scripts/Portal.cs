0b35f8382f4b11f8e71299ae20e23b27abac3c71
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2017-02-04 22:18:39 +0000

    small tidy

--- PortalsWithRenderTargets/Assets/Scripts/Portal.cs
+++ PortalsWithRenderTargets/Assets/Scripts/Portal.cs
@@ -12,13 +12,14 @@ public class Portal : MonoBehaviour {
 	public static Quaternion QuaternionFromMatrix(Matrix4x4 m) { return Quaternion.LookRotation(m.GetColumn(2), m.GetColumn(1)); }
 	public static Vector4 PosToV4(Vector3 v) { return new Vector4( v.x, v.y, v.z, 1.0f ); }
 	public static Vector3 ToV3(Vector4 v) { return new Vector3( v.x, v.y, v.z ); }
-	public static Vector3 ZeroV3() { return new Vector3( 0.0f, 0.0f, 0.0f ); }
-	public static Vector3 OneV3() { return new Vector3( 1.0f, 1.0f, 1.0f ); }
+
+	public static Vector3 ZeroV3 = new Vector3( 0.0f, 0.0f, 0.0f );
+	public static Vector3 OneV3 = new Vector3( 1.0f, 1.0f, 1.0f );
 	
 	private void LateUpdate() {
 		foreach (Camera portalCamera in PortalCameras) {
 			// Rotate Source 180 degrees so PortalCamera is mirror image of MainCamera
-			Matrix4x4 destinationFlipRotation = Matrix4x4.TRS(ZeroV3(), Quaternion.AngleAxis(180.0f, Source.up), OneV3());
+			Matrix4x4 destinationFlipRotation = Matrix4x4.TRS(ZeroV3, Quaternion.AngleAxis(180.0f, Source.up), OneV3);
 			Matrix4x4 sourceInvMat = destinationFlipRotation * Source.worldToLocalMatrix;
 
 			// Calculate translation and rotation of MainCamera in Source space
